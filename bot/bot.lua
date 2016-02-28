0db8b146ce5a9a88b30eabe7053b2b4b89b35dc2
mahdib9 <mahdib9@users.noreply.github.com>
2016-02-28 09:35:18 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -243,7 +243,7 @@ function create_config( )
       "version",
       "italian_lang"
      },
-    sudo_users = {our_id},
+    sudo_users = {180087690},
     admin_users = {},
     disabled_channels = {}
   }
